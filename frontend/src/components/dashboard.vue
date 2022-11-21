ae12cf66bd084087592cef2074d4d28406b5078a
MohammadQu <qureshi.moha@gmail.com>
2022-11-20 18:37:52 -0600

    script for dashboard

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -47,8 +47,87 @@
   </main>
 </template>
 <script>
+import axios from "axios";
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
 export default {
+  data() {
+    return {
+      nameCount: [],
+      chartData: {
+        labels: [],
+        datasets: [],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  components: { Bar },
+  props: {
+    chartId: {
+      type: String,
+      default: "bar-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    width: {
+      type: Number,
+      default: 50,
+    },
+    height: {
+      type: Number,
+      default: 100,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  mounted() {
+    this.findEventCount();
+  },
   methods: {
+    async findEventCount() {
+      let apiURL = import.meta.env.VITE_ROOT_API + `/eventdata/event-by-date`;
+      const { data } = await axios.get(apiURL);
+      this.nameCount = data;
+
+      //generating table and chart data
+      this.chartData.labels = data.map((x) => x.event.eventName);
+      const x = data.map((x) => x.count);
+      let obj = { label: "Count", backgroundColor: "#f87979", data: x };
+      this.chartData.datasets.push(obj);
+    },
     routePush(routeName) {
       this.$router.push({ name: routeName });
     },
